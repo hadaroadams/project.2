e19bcabdf245c784d157d997016b3be0dbc6ff70
hadaroadams <hadaroadams1234@gmail.com>
2022-12-05 09:48:30 +0000

    validation

---
+++ script.js
@@ -0,0 +1,14 @@
+//alert("helos")
+function val(){
+    let name=document.getElementById("name").value
+    let password=document.getElementById("password").value
+    if(name==null||name==""){
+        alert("fill in name");
+    }
+    else if(password.length<6){
+        alert("password needs to be more than 6 character");
+    }
+    else{
+        alert("loading....%");
+    }
+}
